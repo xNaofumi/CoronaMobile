4c8f737846f09f52dfcb7e43242ca6bb69e88bcd
jackf <jackf@DESKTOP-C8LQ2EF>
2021-07-06 11:15:39 +0300

    Добавьте файлы проекта.

---
+++ CoronaMobile/CoronaMobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CoronaMobile.ViewModels;
+using CoronaMobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CoronaMobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ CoronaMobile/CoronaMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CoronaMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CoronaMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
