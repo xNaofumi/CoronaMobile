227a5e85d108c626bbdec148895ad02b6a91cd55
Егор Павлов <pavlov.eo.work@gmail.com>
2021-07-12 18:49:52 +0300

    Removed LoginPage hardcoded class name string.

--- CoronaMobile/CoronaMobile/AppShell.xaml.cs
+++ CoronaMobile/CoronaMobile/AppShell.xaml.cs
@@ -17,7 +17,7 @@ namespace CoronaMobile
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//LoginPage");
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
         }
     }
 }
