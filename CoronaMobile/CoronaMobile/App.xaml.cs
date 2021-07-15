cea124c050faeac9b0c2348ad91b8841fa95865d
Егор Павлов <pavlov.eo.work@gmail.com>
2021-07-15 15:44:21 +0300

    HTTP client added.

--- CoronaMobile/CoronaMobile/App.xaml.cs
+++ CoronaMobile/CoronaMobile/App.xaml.cs
@@ -1,19 +1,41 @@
 ﻿using CoronaMobile.Services;
 using CoronaMobile.Views;
-using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
 
 namespace CoronaMobile
 {
     public partial class App : Application
     {
+        public readonly HttpClient Client;
+        public readonly string ServerUrl;
 
         public App()
         {
             InitializeComponent();
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
+
+            Client = new HttpClient();
+            ServerUrl = "http://194.87.92.52:8080";
+            Client.BaseAddress = new Uri(ServerUrl);
+            CheckServerConnection();
+        }
+
+        private async void CheckServerConnection()
+        {
+            try
+            {
+                string responseBody = await Client.GetStringAsync(ServerUrl);
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.CurrentPage.DisplayAlert("Ошибка подключения к серверу", ex.Message, "OK");
+                await Task.Delay(10000);
+                CheckServerConnection();
+            }
         }
 
         protected override void OnStart()
