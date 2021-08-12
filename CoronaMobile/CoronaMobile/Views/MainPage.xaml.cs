d65227c6f62e4ae542afae27670092cf97d6e19b
Егор Павлов <pavlov.eo.work@gmail.com>
2021-08-12 05:31:17 +0300

    Some refactoring.

--- CoronaMobile/CoronaMobile/Views/MainPage.xaml.cs
+++ CoronaMobile/CoronaMobile/Views/MainPage.xaml.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using CoronaMobile.ViewModels;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace CoronaMobile.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
         }
     }
 }
